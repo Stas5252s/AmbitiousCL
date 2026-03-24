5654d4139a8dca957b8dfbaf91e695344e67d097
Stas <stasir777@gmail.com>
2026-03-24 20:49:19 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import { Github, Mail, Send } from "lucide-react";
 
 const socials = [
-  { icon: Send, label: "TG", href: "https://t.me/ambitias" },
+  { icon: Send, label: "TG", href: "https://t.me/ambitis" },
   { icon: Mail, label: "GM", href: "#" },
   { icon: Github, label: "GH", href: "#" },
 ];
